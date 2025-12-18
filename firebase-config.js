5c17f8cf7952d850eb6a9b376f55ab3630085a62
tislamkanon <165473728+tislamkanon@users.noreply.github.com>
2025-12-18 20:18:14 +0700

    Add files via upload

---
+++ firebase-config.js
@@ -0,0 +1,17 @@
+// Firebase Configuration for DraftaCV Blog
+// Using Firebase v9 modular SDK with CDN
+
+// This file will be loaded as a module
+const firebaseConfig = {
+  apiKey: "AIzaSyCz4Nmsxk4t__Nao2yMxapYNMAeFGdmfAI",  // This is a CLIENT key
+  authDomain: "draftacv-blog-ba799.firebaseapp.com",
+  projectId: "draftacv-blog-ba799",
+  storageBucket: "draftacv-blog-ba799.firebasestorage.app",
+  messagingSenderId: "212660612656",
+  appId: "1:212660612656:web:c805d5569033f1a76547f5",
+  measurementId: "G-KJ1BMN1ZH9"
+};
+
+// Initialize Firebase (will be done in each file)
+
+window.firebaseConfig = firebaseConfig;
